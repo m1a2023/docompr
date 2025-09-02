fda27499045d0353e617c658c91aa446a4cbc99e
m1a2023 <mag2017go@gmail.com>
2025-09-02 15:20:43 +0500

    divided to server and client; added cmakelists

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
-#include "libs/gzstream/gzstream.h"
+
+#include "gzstream.h"
 #include "protos/file.grpc.pb.h"
 #include "protos/file.pb.h"
-
 #include <grpcpp/grpcpp.h>
 
 int main() {
+
     return 0;
 }
---
+++ server/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+#include "gzstream.h"
+#include "protos/file.grpc.pb.h"
+#include "protos/file.pb.h"
+#include <grpcpp/grpcpp.h>
+
+int main() {
+
+    return 0;
+}
